c720728ede6777c84a87fa2d27427cc442123402
Larry Nickerson <lnickers@austin.rr.com>
2017-12-02 21:49:18 -0600

    adde code

---
+++ Classes and Objects/Car.swift
@@ -0,0 +1,36 @@
+//
+//  Car.swift
+//  Classes and Objects
+//
+//  Created by Larry Nickerson on 12/2/17.
+//  Copyright © 2017 Larry Nickerson. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+enum CarType {
+    case Sedan
+    case Coupe
+    case Hatchback
+}
+
+class Car {
+    var typeOfCar : CarType = .Hatchback
+    var colour = "Black"
+    let numberOfSeats : Int =  5
+    
+    
+    init(){
+        
+    }
+    
+    convenience init(customerChosenColour: String) {
+        self.init()
+        colour = customerChosenColour
+    }
+    
+}
+
+
+
--- Classes and Objects/main.swift
+++ Classes and Objects/main.swift
@@ -8,5 +8,19 @@
 
 import Foundation
 
-print("Hello, World!")
+let myCar = Car()
+
+let someRichGuysCar = Car(customerChosenColour: "Gold")
+
+
+print ("Color: \(myCar.colour)")
+print ("Seats: \(myCar.numberOfSeats)")
+print("CarType: (\(myCar.typeOfCar)) which is has a hash of: \(myCar.typeOfCar.hashValue)")
+
+
+print ("Color:   \(someRichGuysCar.colour)")
+print ("Seats:   \(someRichGuysCar.numberOfSeats)")
+print("CarType: (\(someRichGuysCar.typeOfCar)) which is has a hash of: \(myCar.typeOfCar.hashValue)")
+
+
 
